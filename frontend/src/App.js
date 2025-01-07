d849974f1a0e20b0ecf308dfcb3ce4047fb42c5b
adarssssh <adarsht06@gmail.com>
2025-01-07 16:22:30 +0530

    Added Vendor Details Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Homepage from './components/landing/Homepage';
 import ListVenue from './components/venue/listVenue';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Decorators from './components/decorators/Decorators';
+import VendorDetails from './components/venue/VendorDetails';
 
 
 
@@ -22,6 +23,7 @@ function App() {
         <Route path= '/register' element= {<Register />} />
         <Route path = '/venue' element = {<ListVenue />} />
         <Route path= '/decorators' element={<Decorators/>} />
+        <Route path="/vendor/:vendorId" element={<VendorDetails />} />
         
 
       </Routes>
--- frontend/src/components/venue/VenueContainer.js
+++ frontend/src/components/venue/VenueContainer.js
@@ -1,21 +1,44 @@
-import styles from '../venue/venue.css'
+import { useState } from 'react';
+import styles from '../venue/venue.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const VenueContainer = (data) => {
+    
+    const [vendorDetails, setVendorDetails] = useState(null);
+    const navigate = useNavigate()
+    const vendor_id_URL = `http://127.0.0.1:8000/venue/vendor/`
 
-    console.log(data.data)
+    
 
     let img = data.data.profile_pic_url
     const image = img.replace("%%","250")
 
+    const handleVenue = async () => {
+        const vendorId = data?.data?.vendor_id;
     
+        if (vendorId) {
+          try {
+            
+            
+            // const response = await axios.get(vendor_id_URL+vendorId);
+            
+            navigate(`/vendor/${vendorId}`);
+          } catch (err) {
+            console.error("Error fetching vendor data", err);
+          }
+        } else {
+          console.error("Vendor ID not found");
+        }
+      };
     
     
     return(
-        <>
+        <div className="Venuecontainer" onClick={handleVenue}>
         <div className="Venuecards styles_containerV2__1_U51">
             <div className='vc-img'>
             <img src = {image}/>
-            </div>
+        </div>
             
             <div className='vc-venue-details '>
                 <div className='vc-venue-name'>{data?.data?.name}</div>
@@ -30,7 +53,7 @@ const VenueContainer = (data) => {
             
             
         </div>
-        </>
+        </div>
     )
 }
 
